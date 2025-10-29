ae8d887a0805ec82190078600fe7028fe3edc057
MD.RIYAD KHAN <141185765+riduan007@users.noreply.github.com>
2025-10-29 13:55:13 +0600

    Add files via upload

---
+++ lab_t07.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string first, last;
+
+    cout << "First name: ";
+    cin >> first;
+
+    cout << "Last name: ";
+    cin >> last;
+
+    cout << "Full name: " << first << " " << last << endl;
+
+    return 0;
+}
